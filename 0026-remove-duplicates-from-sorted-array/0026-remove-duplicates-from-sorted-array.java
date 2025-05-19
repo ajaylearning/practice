6f4ccc11d9ab7157d47d76d5550511f88f0c90f0
ajaylearning <ajaylearning2023@gmail.com>
2025-05-19 12:08:13 +0530

    Time: 0 ms (100%), Space: 45.1 MB (23.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,25 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n= nums.length;
-        ArrayList<Integer> temp= new ArrayList<>();
-        temp.add(nums[0]);
-        for(int i =1;i<n;i++){
-           if(nums[i-1]<nums[i]){
-               temp.add(nums[i]);
+        // ArrayList<Integer> temp= new ArrayList<>();
+        // temp.add(nums[0]);
+        // for(int i =1;i<n;i++){
+        //    if(nums[i-1]<nums[i]){
+        //        temp.add(nums[i]);
+        //    }
+        // }
+        
+        // int k = temp.size();
+        // for(int i=0;i<k;i++){
+        //     nums[i]= temp.get(i);
+        // }
+        int i=0;
+        for(int j =0;j<n;j++){
+           if(nums[i]<nums[j]){
+                  i++;
+                  nums[i]=nums[j];
            }
         }
-        
-        int k = temp.size();
-        for(int i=0;i<k;i++){
-            nums[i]= temp.get(i);
-        }
-        return k;
+        return i+1;
     }
 }
